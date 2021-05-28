a84be8d60feff2002a5e3cd616f2b95078a0e137
Thiago de Freitas Gonçalves <48700732+ThiagoFreitasFreelancer@users.noreply.github.com>
2021-05-28 09:13:07 -0300

    estudos

---
+++ aula21/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+const a = 0;
+const b = false;
+const c = NaN;
+const d = undefined;
+const e = '';
+
+//Vai retornar o ultimo valor falso, por que todos são falsos
+console.log(a || b || c || e || d);
+
