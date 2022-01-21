2d0ccb172a419e2c5f873269301d2d1ae925ccba
thiago.freitas <thiago.freitas@cepedi.org.br>
2022-01-20 22:02:21 -0300

    upadte

--- aula39/index.js
+++ aula39/index.js
@@ -1,6 +1,45 @@
 
+function FizzBuzz(a){
 
+    if(typeof a !== 'number') return a
+    if(a % 3 === 0 && a % 5 === 0) return console.log('FizzBuzz');
+    if(a % 3 === 0) return console.log('Fizz');
+    if(a % 5 === 0) return console.log('Buzz');
 
-const FizzBuss = () => {
-    
 }
+
+
+const Fizz = (a) => {
+
+    let type = typeof a 
+
+    if(type === "number"){
+            
+        let res = a % 3;
+        let res2 = a % 5;
+
+        if(res == 0){
+            if(res2 == 0){
+                return console.log("FizzBuzz");
+            }
+            return console.log("Fizz");
+        }else if(res2 == 0){
+            
+            return console.log("Buzz");
+        }
+
+        return console.log(a);
+
+    }
+
+    return console.log(a, "String")
+
+
+}
+
+for (let i = 0; i < 100 ; i++){
+
+    FizzBuzz(i);
+
+}
+
---
+++ aula40/index.js
@@ -0,0 +1,13 @@
+
+
+// try{
+//     console.log(naoExiste)
+// } catch(err){
+//     console.log('eita')
+// }
+
+/**
+ * para lançar meus proprios erro em java scrpit
+ * eu preciso utilizar o "trow()" ou se quiser um erro
+ * igual o do javascript seria "trow new Error()" 
+ */
